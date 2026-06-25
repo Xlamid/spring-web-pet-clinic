c738952084263f3d720baa6979353e4ce54538bc
Xlamid <doronin52.com@gmail.com>
2026-06-25 11:20:27 +0600

    init commit

---
+++ src/main/java/git/xlamid/springwebpetclinic/SpringWebPetClinicApplication.java
@@ -0,0 +1,13 @@
+package git.xlamid.springwebpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/git/xlamid/springwebpetclinic/SpringWebPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package git.xlamid.springwebpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
